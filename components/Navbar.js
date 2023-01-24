fd59fd1bf058baa54e811af0e8939df6741b2e50
laluheri <heri170890@gamil.com>
2023-01-24 15:45:10 +0800

    fix youtube link

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React, { useState } from "react";
-import YouTubeLink from "./YouTubeLink";
+import YouTubeLink from "./YoutubeLink";
 
 export default function Navbar() {
   const [isActive, setIsActive] = useState(false);
